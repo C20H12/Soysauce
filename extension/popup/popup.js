8451ba8a074a8e2dac17536ec2cac882a4db1dea
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2024-02-04 01:21:39 -0700

    added settings controls

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -5,6 +5,71 @@ document.querySelectorAll("[data-action]").forEach(action => {
   });
 })
 
+// check if a saved data object exists, if not define the defaults
+chrome.storage.local.get(["soysauceSavedData"], async (data) => {
+  if (!data.soysauceSavedData) {
+    const defaultData = {
+      word_min: 40,
+      word_max: 80,
+      question_min: 10,
+      question_max: 15,
+      color_question: "#f0fff1",
+      color_quiz: "#f0fff1",
+    }
+    await chrome.storage.local.set({ soysauceSavedData: defaultData });
+  }
+});
+
+document.querySelector("[data-settings=open]").addEventListener('click', async () => {
+  // show settings area
+  document.querySelector("div.settings").style.display = "block";
+  document.body.style.width = '30rem';
+  // prefill the fields with saved data
+  const savedData = await chrome.storage.local.get(["soysauceSavedData"]);
+  document.querySelectorAll("input[data-setting-property]").forEach(inp => {
+    inp.value = savedData.soysauceSavedData[inp.dataset.settingProperty];
+  });
+})
+
+document.querySelector("[data-settings=close]").addEventListener('click', async () => {
+  // close settings area
+  document.querySelector("div.settings").style.display = "none";
+  document.body.style.width = 'fit-content';
+})
+
+document.querySelector("[data-settings=save]").addEventListener('click', async () => {
+  // verify values
+  for (const section of document.querySelectorAll("[data-setting-numbers]")) {
+    const minInput = section.querySelector("input[data-setting-property*=_min]");
+    const maxInput = section.querySelector("input[data-setting-property*=_max]");
+    const minInputValue = parseInt(minInput.value);
+    const maxInputValue = parseInt(maxInput.value);
+    if (minInputValue > maxInputValue) {
+      alert("Error: max is less than min at one or more sections");
+      return;
+    }
+    if (minInputValue < parseInt(minInput.min) || minInputValue > parseInt(minInput.max) ) {
+      alert(`Error: at ${minInput.dataset.settingProperty},  must be between ${minInput.min} and ${minInput.max}`);
+      return;
+    }
+    if (maxInputValue < parseInt(maxInput.min) || maxInputValue > parseInt(maxInput.max) ) {
+      alert(`Error: at ${maxInput.dataset.settingProperty},  must be between ${maxInput.min} and ${maxInput.max}`);
+      return;
+    }
+  }
+
+  // save data from the input fields
+  const dataToSave = {};
+  document.querySelectorAll("input[data-setting-property]").forEach(inp => {
+    dataToSave[inp.dataset.settingProperty] = inp.value;
+  });
+  await chrome.storage.local.set({ soysauceSavedData: dataToSave });
+  
+  // close settings area
+  document.querySelector("div.settings").style.display = "none";
+  document.body.style.width = 'fit-content';
+})
+
 async function sendStatus(name) {
   try {
     // need to get the tab's id to send the message to
